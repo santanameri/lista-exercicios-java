f5c3becec83ee3bd4ecebb2af8dc9413b250cd15
Merielly Santana <80992709+santanameri@users.noreply.github.com>
2022-06-28 20:17:21 -0300

    duas-médias

---
+++ MediaAri.java
@@ -0,0 +1,20 @@
+
+public class MediaAri {
+	public static void main(String[] args) {
+	int a, b, c, d, e, f, media1, media2 = 0;
+	
+	a = 4;
+	b = 5;
+	c = 6;
+	d = 7;
+	e = 8;
+	f = 9;
+	media1 = (a + b + c) / 3;
+	
+	System.out.println("O valor da média aritmética dos números 4, 5 e 6 é: " + media1);
+	
+	media2 = (d + e + f) / 3;
+	
+	System.out.println("O valor da média aritmética dos números 7, 8 e 9 é: " + media2);
+	}
+}
