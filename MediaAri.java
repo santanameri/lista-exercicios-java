f08d9a3e32c3cd382fbd9d4377850ca4f4211659
Merielly Santana <80992709+santanameri@users.noreply.github.com>
2022-06-28 20:18:48 -0300

    update

--- MediaAri.java
+++ MediaAri.java
@@ -1,7 +1,7 @@
 
 public class MediaAri {
 	public static void main(String[] args) {
-	int a, b, c, d, e, f, media1, media2 = 0;
+	int a, b, c, d, e, f, media1, media2, media3 = 0;
 	
 	a = 4;
 	b = 5;
@@ -16,5 +16,9 @@ public class MediaAri {
 	media2 = (d + e + f) / 3;
 	
 	System.out.println("O valor da média aritmética dos números 7, 8 e 9 é: " + media2);
+	
+	media3 = (media1 + media2) / 2;
+	
+	System.out.println("O valor da média das médias anteriores é de: " + media3);
 	}
 }
